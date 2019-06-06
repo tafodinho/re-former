cde880a4cd7cf3eddaee158233d174c7013a8bfb
offorma <offorma@gmail.com>
2019-06-07 00:26:13 +0100

    finished edit functionality with flash msg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,25 @@ class UsersController < ApplicationController
           render :new
         end
     end
+    def edit
+        set_user
+    end
+    def update
+        set_user
+        @user.update(user_params)   
+        if @user.valid?     
+            flash[:success] = "User updated successfully"
+          redirect_to new_user_path
+        else
+          flash[:errors] = @user.errors.full_messages
+          redirect_to edit_user_path(@user)
+        end
+    end
     private
         def user_params
             params.require(:user).permit(:username,:email,:password)
         end
+        def set_user  
+            @user = User.find(params[:id])
+          end
 end
